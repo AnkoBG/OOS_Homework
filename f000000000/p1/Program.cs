b455d049748c3d94183e57f06208e59dfa7e95e3
Angel Neev <33812357+AnkoBG@users.noreply.github.com>
2019-04-09 01:10:43 +0300

    Added Neli's homework

---
+++ f000000000/p1/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using d1;
+
+namespace p1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            foreach (var arg in args)
+            {
+                if (arg == "--gen")
+                    Class1.Gen();
+                else if (arg == "--sort")
+                    Class1.Sort();
+                else if (arg == "--view")
+                    Class1.View();
+
+            }
+        }
+    }
+}
--- f181217022/p2/Form1.cs
+++ f181217022/p2/Form1.cs
@@ -21,53 +21,44 @@ namespace p2
         private void GenButton_Click(object sender, EventArgs e)
         {
             string args = "-gen " + textBox1.Text + " " + numericUpDown1.Value.ToString();
-            Process p = ConfigureProcess(args);
-            p.Start();
-            string output = p.StandardOutput.ReadToEnd();
-            label3.Text = output != "" ? output : label3.Text;
-            p.WaitForExit();
+            ConfigureProcess(args);
         }
 
         private void SortButton_Click(object sender, EventArgs e)
         {
             string args = "-sort " + textBox1.Text;
-            Process p = ConfigureProcess(args);
-            p.Start();
-            string output = p.StandardOutput.ReadToEnd();
-            label3.Text = output != "" ? output : label3.Text;
-            p.WaitForExit();
+            ConfigureProcess(args);
         }
 
         private void SortDescButton_Click(object sender, EventArgs e)
         {
             string args = "-sortdesc " + textBox1.Text;
-            Process p = ConfigureProcess(args);
-            p.Start();
-            string output = p.StandardOutput.ReadToEnd();
-            label3.Text = output != "" ? output : label3.Text;
-            p.WaitForExit();
+            ConfigureProcess(args);
         }
 
         private void ViewButton_Click(object sender, EventArgs e)
         {
             string args = "-view " + textBox1.Text;
-            Process p = ConfigureProcess(args);
-            p.Start();
-            string output = p.StandardOutput.ReadToEnd();
-            label3.Text = output != "" ? output : label3.Text;
-            p.WaitForExit();
+            ConfigureProcess(args);
         }
 
 
-        public Process ConfigureProcess(string args)
+        public void ConfigureProcess(string args)
         {
             Process p = new Process();
+
             p.StartInfo.FileName = "p1.exe";
             p.StartInfo.Arguments = args;
             p.StartInfo.UseShellExecute = false;
             p.StartInfo.RedirectStandardOutput = true;
             p.StartInfo.CreateNoWindow = true;
-            return p;
+            p.Start();
+
+            string output = p.StandardOutput.ReadToEnd();
+
+            label3.Text = output != "" ? output : label3.Text;
+
+            p.WaitForExit();
         }
 
     }
