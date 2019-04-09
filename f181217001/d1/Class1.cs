3ae4c24034a74bb92f94ff03044c179fa9149c39
Angel Neev <33812357+AnkoBG@users.noreply.github.com>
2019-04-09 13:22:25 +0300

    Fixed name and added required "tester"

---
+++ f181217001/d1/Class1.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace d1
+{
+    public static class Class1
+    {
+        private readonly static string path = "file.txt";
+
+        public static void Gen()
+        {
+            Random r = new Random();
+            List<string> numList = new List<string>();
+
+            for (int i = 0; i < 10; i++)
+            {
+                numList.Add(r.Next(100).ToString());
+            }
+
+            File.WriteAllLines(path, numList);
+            Console.WriteLine(path + " generated!");
+        }
+
+        public static void View()
+        {
+            if (File.Exists(path))
+                Console.WriteLine(File.ReadAllText(path));
+            else
+                Console.WriteLine(path + " does not exist.");
+        }
+
+        public static void Sort()
+        {
+            List<int> nums = File.ReadAllLines(path).Select(int.Parse).ToList();
+
+            nums.Sort();
+
+            File.WriteAllLines(path, nums.ConvertAll<string>(x => x.ToString()));
+
+            Console.WriteLine(path + " sorted!");
+        }
+    }
+}
---
+++ f181217001/t1/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Diagnostics;
+
+namespace t1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            while (true)
+            {
+                Console.WriteLine("Please write the arguments you wish p1 to execute");
+                string arguments = Console.ReadLine();
+
+                Process p = new Process();
+
+                p.StartInfo.FileName = "p1.exe";
+                p.StartInfo.Arguments = arguments;
+                p.StartInfo.UseShellExecute = false;
+                p.StartInfo.CreateNoWindow = true;
+                p.StartInfo.RedirectStandardOutput = true;
+                p.Start();
+                Console.WriteLine(p.StandardOutput.ReadToEnd());
+                p.WaitForExit();
+            }
+        }
+    }
+}
